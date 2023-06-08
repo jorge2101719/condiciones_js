98343c5e8c8d3b53e5adc2bdbe1bc031d7b9f523
jorge2101719 <rusosky01@gmail.com>
2023-06-08 14:44:27 -0400

    se añaden algunos estilos básicos a la imagen y se hace pequeña modificación al script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
+// Parte 1
+
 let img = document.querySelector('#imagen_automovil');
 let contador = 0;
 
 img.addEventListener('click', () => {
     if (contador % 2 == 0) {
-        console.log('dentro de la función');
         img.style.border = '2px solid red';
         contador += 1;
     } else {
