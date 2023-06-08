9d61fb95d3d3ecd0b08ea91ae51c7c11d5a44364
jorge2101719 <rusosky01@gmail.com>
2023-06-08 14:51:37 -0400

    se incluye etiqueta p contenedora de un mensaje junto a la imagen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 // Parte 1
 
 let img = document.querySelector('#imagen_automovil');
+let comentario = document.querySelector('#comentario');
 let contador = 0;
 
 img.addEventListener('click', () => {
     if (contador % 2 == 0) {
         img.style.border = '2px solid red';
+        comentario.innerHTML = 'Con borde';
         contador += 1;
     } else {
         img.style.border = '0';
+        comentario.innerHTML = 'Sin borde';
         contador += 1;
     }
 });
