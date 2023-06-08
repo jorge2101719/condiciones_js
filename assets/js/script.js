f7caa08dd1b2f43a2c31300bfed58bd1d0cc21e1
jorge2101719 <rusosky01@gmail.com>
2023-06-08 16:43:42 -0400

    algunas correcciones en html y css; implemento lógica en script.js de la Parte 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,40 @@ img.addEventListener('click', () => {
         contador += 1;
     }
 });
+
+
+// Parte 2
+
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+let input3 = document.querySelector('#input3');
+let aviso = document.querySelector('#aviso');
+let verificar = document.querySelector('#verificar');
+let suma = 0;
+
+verificar.addEventListener('click', () => {
+    if (input1.value > 0 && input2.value > 0 && input3.value > 0) {
+        suma = Number(input1.value) + Number(input2.value) + Number(input3.value);
+        if (suma > 10) {
+            aviso.innerHTML = 'Tienes demasiados Stickers ' + '(' + suma + ')';
+            input1.value = '';
+            input2.value = '';
+            input3.value = '';
+            // suma = 0;
+        } else {
+            aviso.innerHTML = `Tienes ${suma} stickers`;
+        }
+    } else {
+        console.log(input1);
+        aviso.innerHTML = 'uno (o más) número(s) es(son) incorrecto(s)';
+        input1.value = '';
+        input2.value = '';
+        input3.value = '';
+        suma = 0;
+        console.log(input1)
+    }
+});
+
+
+
+// Parte 3
